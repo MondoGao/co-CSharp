f9c5154467c9607f19d38a889eb6746fb9feb761
Mondo Gao <mondogao@gmail.com>
2018-01-08 16:38:06 +0800

    feat(Homework3): add api client

---
+++ Homework3_APIClient/Douban.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Homework3_APIClient
+{
+    partial class Douban
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxMain = new System.Windows.Forms.ListBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxMain
+            // 
+            this.listBoxMain.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listBoxMain.FormattingEnabled = true;
+            this.listBoxMain.ItemHeight = 24;
+            this.listBoxMain.Location = new System.Drawing.Point(0, 0);
+            this.listBoxMain.Name = "listBoxMain";
+            this.listBoxMain.Size = new System.Drawing.Size(655, 196);
+            this.listBoxMain.TabIndex = 0;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.richTextBox.Location = new System.Drawing.Point(0, 196);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(655, 136);
+            this.richTextBox.TabIndex = 3;
+            this.richTextBox.Text = "";
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonRefresh.Location = new System.Drawing.Point(0, 332);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(655, 67);
+            this.buttonRefresh.TabIndex = 4;
+            this.buttonRefresh.Text = "获取数据";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // Douban
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(655, 455);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.listBoxMain);
+            this.Name = "Douban";
+            this.Text = "Douban";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxMain;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button buttonRefresh;
+    }
+}
+
---
+++ Homework3_APIClient/Douban.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework3_APIClient
+{
+    public partial class Douban : Form
+    {
+        public Douban()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            getInTheatersData();
+        }
+
+        private class Movie
+        {
+            public string Title { get; set; }
+        }
+
+        private async void getInTheatersData()
+        {
+            HttpClient client = new HttpClient();
+
+            richTextBox.Text = "Loading...";
+
+            var resp = await client.GetAsync(@"http://api.douban.com/v2/movie/in_theaters");
+
+            var str = await resp.Content.ReadAsStringAsync();
+
+            JObject jObject = JObject.Parse(str);
+            List<Movie> movies = new List<Movie>();
+
+            foreach (var subj in jObject["subjects"].Children().ToList())
+            {
+                movies.Add(subj.ToObject<Movie>());
+            }
+
+            listBoxMain.Items.Clear();
+
+            foreach (var m in movies)
+            {
+                Console.WriteLine(m.Title);
+                listBoxMain.Items.Add(m.Title);
+            }
+
+            richTextBox.Text = "Finish";
+        }
+    }
+}
