546b93290e94542589d40d4af3a266d3849d1cd9
Mondo Gao <mondogao@gmail.com>
2018-01-08 15:37:11 +0800

    feat(Homework3): add WebAPI

---
+++ Homework3_WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Homework3_WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Homework3_WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Homework3_WebAPI.WebApiApplication" Language="C#" %>
