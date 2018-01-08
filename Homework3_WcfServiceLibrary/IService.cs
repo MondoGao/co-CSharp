8811b802174f681901b9c84ea1d39d897b168331
Mondo Gao <mondogao@gmail.com>
2018-01-08 15:02:49 +0800

    feat(Homework3): finish WCF Service

---
+++ Homework3_WcfServiceLibrary/IService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Homework1.Models;
+
+namespace Homework3_WcfServiceLibrary
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        StudentType getStudent(int id);
+        
+        [OperationContract]
+        List<StudentType> getAllStudents();
+
+        [OperationContract]
+        Boolean addStudent(StudentType stuT);
+
+        [OperationContract]
+        Boolean deleteStudent(int id);
+    }
+
+    [DataContract]
+    public class StudentType
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public Sex Sex { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+        public StudentType() { }
+
+        public StudentType(Student stu)
+        {
+            this.Id = stu.Id;
+            this.Name = stu.Name;
+            this.Age = stu.Age;
+            this.Sex = stu.Sex;
+        }
+    }
+}
---
+++ Homework3_WcfServiceLibrary/Service.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using static Homework1.DAL.HomeworkDAL;
+
+namespace Homework3_WcfServiceLibrary
+{
+    public class Service : IService
+    {
+        public bool addStudent(StudentType stuT)
+        {
+            try {
+                StudentDAL.addStudent(stuT.Name, stuT.Age, stuT.Sex);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool deleteStudent(int id)
+        {
+            try
+            {
+                StudentDAL.deleteStudent(id);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public List<StudentType> getAllStudents()
+        {
+            var stus = StudentDAL.getAllStudents();
+            var stuTs = new List<StudentType>();
+
+            foreach (var stu in stus)
+            {
+                stuTs.Add(new StudentType(stu));
+            }
+
+            return stuTs;
+        }
+
+        public StudentType getStudent(int id)
+        {
+            var stu = StudentDAL.getStudent(id);
+
+            if (stu == null)
+            {
+                throw new ArgumentNullException("no student");
+            }
+
+            return new StudentType(stu);
+        }
+    }
+}
